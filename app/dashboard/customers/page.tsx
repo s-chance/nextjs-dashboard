bfd896a4a4d19faa7705489e73cbe50c016d685f
s-chance <entropy.tree@outlook.com>
2024-06-16 22:25:34 +0800

    feat(*): customers page and functionality

--- app/dashboard/customers/page.tsx
+++ app/dashboard/customers/page.tsx
@@ -1,11 +1,35 @@
 import { Metadata } from 'next';
+import CustomersTable from '@/app/ui/customers/table';
+import Pagination from '@/app/ui/invoices/pagination';
+import { fetchCustomersPages } from '@/app/lib/data';
 
 export const metadata: Metadata = {
   title: 'Customers',
 };
 
-const Page = () => {
-  return <p>Customers Page</p>;
+const Page = async ({
+  searchParams,
+}: {
+  searchParams?: {
+    query?: string;
+    page?: string;
+  };
+}) => {
+  const query = searchParams?.query || '';
+  const currentPage = Number(searchParams?.page) || 1;
+
+  const totalPages = await fetchCustomersPages(query);
+
+  return (
+    <main>
+      {/* <Suspense key={query + currentPage} fallback={<InvoicesTableSkeleton />}> */}
+      <CustomersTable query={query} currentPage={currentPage} />
+      {/* </Suspense> */}
+      <div className="mt-5 flex w-full justify-center">
+        <Pagination totalPages={totalPages} />
+      </div>
+    </main>
+  );
 };
 
 export default Page;
