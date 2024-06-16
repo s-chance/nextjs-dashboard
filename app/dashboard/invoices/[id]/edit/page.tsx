cac13035ea677b5fbfae9e97e58933a2a6a21ac4
s-chance <entropy.tree@outlook.com>
2024-06-16 18:23:58 +0800

    feat(*): handling 404 errors in edit page

--- app/dashboard/invoices/[id]/edit/page.tsx
+++ app/dashboard/invoices/[id]/edit/page.tsx
@@ -1,6 +1,7 @@
 import Breadcrumbs from '@/app/ui/invoices/breadcrumbs';
 import Form from '@/app/ui/invoices/edit-form';
 import { fetchInvoiceById, fetchCustomers } from '@/app/lib/data';
+import { notFound } from 'next/navigation';
 
 const Page = async ({ params }: { params: { id: string } }) => {
   const id = params.id;
@@ -8,6 +9,12 @@ const Page = async ({ params }: { params: { id: string } }) => {
     fetchInvoiceById(id),
     fetchCustomers(),
   ]);
+
+  if (!invoice) {
+    // http://localhost:3000/dashboard/invoices/2e94d1ed-d220-449f-9f11-f0bbceed9645/edit
+    // vist a resource that does not exist
+    notFound();
+  }
   return (
     <main>
       <Breadcrumbs
